90affbe8705dd1f8fffe40659870695a612f5369
Mihhail Matišinets <mihhail.matisinets@testdevlab.com>
2021-02-03 16:11:45 +0200

    Added method to set default headers, added todos

--- src/main/java/loadero/LoaderoClient.java
+++ src/main/java/loadero/LoaderoClient.java
@@ -1,12 +1,11 @@
 package loadero;
-import com.google.gson.Gson;
-import loadero.model.LoaderoTestDescription;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpHeaders;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpUriRequest;
 import org.apache.http.client.methods.RequestBuilder;
+import org.apache.http.entity.HttpEntityWrapper;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
@@ -18,32 +17,28 @@ public class LoaderoClient {
     private static final String LOADERO_API_TOKEN = System.getenv("LOADERO_API_TOKEN");
     private static final String PROJECT_ID = "5040";
     private static final String TEST_ID = "6866";
+    // TODO: create some generic method to create uris.
+    private static final URI uri = URI.create(BASE_URL+"/projects/"+PROJECT_ID+"/tests/"+TEST_ID);
 
     /**
-     * TODO: probably should be private helper method.
-     * getRequest() takes in two params and returns HttpUriRequest object
-     * with predefined headers such as Accept, Content-Type, Authorization.
-     *
-     * @param projectId - ID of the Loadero project
-     * @param testId - ID of the Loadero test
-     * @return
+     * Sets some default headers for Http methods.
+     * @param req - Http request for which set default headers
      */
-    public static HttpUriRequest getRequest(String projectId, String testId) {
-        URI uri = URI.create(BASE_URL+"/projects/"+projectId+"/tests/"+testId);
-        HttpUriRequest req = RequestBuilder.get(uri).build();
+    private static void setDefaultHeaders(HttpUriRequest req) {
         req.setHeader(HttpHeaders.ACCEPT, "*/*");
         req.setHeader(HttpHeaders.CONTENT_TYPE, "application/json");
         req.setHeader(HttpHeaders.AUTHORIZATION, "LoaderoAuth " + LOADERO_API_TOKEN);
-        return req;
     }
 
     /**
-     * Returns test description of the specified project and test.
+     * TODO: make return LoaderoTestDescription object.
+     * Returns test description for the specific test in specific project.
      * @param projectId
      * @param testId
      */
     public static void getTestDescription(String projectId, String testId) {
-        HttpUriRequest get = getRequest(projectId, testId);
+        HttpUriRequest get = RequestBuilder.get(uri).build();
+        setDefaultHeaders(get);
         // Try-catch with resources statement that will close
         // everything for us after we are done.
         try (CloseableHttpClient client = HttpClients.custom().build();
@@ -55,16 +50,12 @@ public class LoaderoClient {
         }
     }
 
-    public static void main(String[] args) {
-        LoaderoTestDescription test = new LoaderoTestDescription(
-                "Name",
-                "mode",
-                "",
-                10,
-                "",
-                10
-        );
+    // TODO
+    public static void updateTestDescription(String projectId, String testId) {
 
+    }
+
+    public static void main(String[] args) {
         getTestDescription(PROJECT_ID, TEST_ID);
     }
 }
