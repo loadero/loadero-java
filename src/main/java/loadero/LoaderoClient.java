62e61822170c83b0748f310b84331a08ce17a984
Mihhail Matišinets <mihhail.matisinets@testdevlab.com>
2021-02-04 15:16:05 +0200

    Added group id variable for testing

--- src/main/java/loadero/LoaderoClient.java
+++ src/main/java/loadero/LoaderoClient.java
@@ -1,6 +1,7 @@
 package loadero;
 
 import loadero.controller.LoaderTestController;
+import loadero.controller.LoaderoGroupController;
 import loadero.model.LoaderoTestOptions;
 import java.net.URI;
 
@@ -11,28 +12,38 @@ public class LoaderoClient {
     private static final String LOADERO_API_TOKEN = System.getenv("LOADERO_API_TOKEN");
     private static final String PROJECT_ID = "5040";
     private static final String TEST_ID = "6866";
+    private static final String GROUP_ID = "49033";
     // TODO: create some generic method to create uris.
     private static final URI uri =
             URI.create(BASE_URL+"/projects/"+PROJECT_ID+"/tests/"+TEST_ID+"/");
 
     public static void main(String[] args) {
         // REST controller for tests
-        LoaderTestController testController = new LoaderTestController(uri, LOADERO_API_TOKEN);
+        LoaderTestController testController = new LoaderTestController(
+                uri,
+                LOADERO_API_TOKEN,
+                PROJECT_ID,
+                TEST_ID);
+        LoaderoGroupController groupController = new LoaderoGroupController(
+                uri,
+                LOADERO_API_TOKEN,
+                PROJECT_ID,
+                TEST_ID,
+                GROUP_ID);
         LoaderoTestOptions test = new LoaderoTestOptions(
                 "new name 5", 30,
                 10, "performance",
                 "random", "src/main/resources/test.js");
 
         // Retrieving current test from loadero
-        LoaderoTestOptions currentTest = testController.getTestDescription(PROJECT_ID, TEST_ID);
+        LoaderoTestOptions currentTest = testController.get();
         System.out.println("Current test: " + currentTest);
-//        System.out.println("Setting new name and script");
+        System.out.println("Setting new name and script");
         // Setting new params
         currentTest.setName("new test 6");
         currentTest.setScript("src/main/resources/test1.js");
-        // Sending updated test options to Loadero
-        LoaderoTestOptions updatedTest = testController.updateTestDescription(PROJECT_ID, TEST_ID, currentTest);
-
+//        // Sending updated test options to Loadero
+        LoaderoTestOptions updatedTest = testController.update(currentTest);
         System.out.println("Updated test: " + updatedTest);
     }
 }
