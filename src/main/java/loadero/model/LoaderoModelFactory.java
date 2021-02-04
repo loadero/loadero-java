7c84c237b91f7d560bf971cd7c786debb8a2609b
Mihhail Matišinets <mihhail.matisinets@testdevlab.com>
2021-02-04 16:58:54 +0200

    Created factory class to abstract creation of concrete Loadero objects

--- src/main/java/loadero/model/LoaderoModelFactory.java
+++ src/main/java/loadero/model/LoaderoModelFactory.java
@@ -1,2 +1,13 @@
-package loadero.model;public class LoaderoModelFactory {
+package loadero.model;
+
+public class LoaderoModelFactory implements LoaderoModel {
+
+    public LoaderoModel getLoaderoModel(LoaderoType type) {
+        switch (type) {
+            case LOADERO_TEST: return new LoaderoTestOptions();
+            case LOADERO_GROUP: return new LoaderoGroup();
+            case LOADERO_PARTICIPANT: return new LoaderoParticipant();
+            default: return null;
+        }
+    }
 }
