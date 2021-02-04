916f754562f84231b43982223dccd5bab9901eca
Mihhail Matišinets <mihhail.matisinets@testdevlab.com>
2021-02-04 13:17:06 +0200

    Defined fields for Loadero groups and participants

--- src/main/java/loadero/model/LoaderoGroup.java
+++ src/main/java/loadero/model/LoaderoGroup.java
@@ -1,4 +1,21 @@
 package loadero.model;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.List;
+
+@Getter
+@Setter
+@ToString
 public class LoaderoGroup {
+    private String name;
+    private int count;
+    private List<LoaderoParticipant> loaderoParticipantList;
+
+    public LoaderoGroup(String name, int count) {
+        this.name = name;
+        this.count = count;
+    }
 }
--- src/main/java/loadero/model/LoaderoParticipant.java
+++ src/main/java/loadero/model/LoaderoParticipant.java
@@ -1,4 +1,19 @@
 package loadero.model;
 
+import com.google.gson.annotations.SerializedName;
+import lombok.Data;
+
+@Data
 public class LoaderoParticipant {
+    private String name;
+    @SerializedName("group_id")
+    private long groupId;
+    private int count;
+    @SerializedName("compute_unit")
+    private String computeUnit;
+    private String browser;
+    private String network;
+    private String location;
+    @SerializedName("media_type")
+    private String mediaType;
 }
