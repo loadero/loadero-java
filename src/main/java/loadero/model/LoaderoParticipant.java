b61d43a25594b8211f4de9b65b98615cd4729359
Mihhail Matišinets <mihhail.matisinets@testdevlab.com>
2021-02-04 17:28:19 +0200

    removed groupId field

--- src/main/java/loadero/model/LoaderoParticipant.java
+++ src/main/java/loadero/model/LoaderoParticipant.java
@@ -6,8 +6,6 @@ import lombok.Data;
 @Data
 public class LoaderoParticipant implements LoaderoModel {
     private String name;
-    @SerializedName("group_id")
-    private long groupId;
     private int count;
     @SerializedName("compute_unit")
     private String computeUnit;
