b252906f0be6c5de5a4d700366e17848b88dc7c2
Mihhail Matišinets <mihhail.matisinets@testdevlab.com>
2021-02-04 10:55:34 +0200

    Added class for helpers methods

--- src/main/java/loadero/LoaderoClientUtils.java
+++ src/main/java/loadero/LoaderoClientUtils.java
@@ -1,2 +1,41 @@
-package loadero.model;public class LoaderoClientUtils {
+package loadero;
+
+import com.google.gson.Gson;
+import loadero.model.LoaderoTestDescription;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpHeaders;
+import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.util.EntityUtils;
+
+import java.util.Objects;
+
+public class LoaderoClientUtils {
+    private static final Gson gson = new Gson();
+    protected static boolean checkNull(Object test) {
+        return Objects.isNull(test);
+    }
+    // Converts JSON from response into LoaderTestDescription object
+    protected static LoaderoTestDescription jsonToTestDescr(HttpEntity entity) {
+        LoaderoTestDescription test = null;
+        try {
+            test = gson.fromJson(EntityUtils.toString(entity), LoaderoTestDescription.class);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return test;
+    }
+
+    protected static String testDescrToJson(LoaderoTestDescription test) {
+        return gson.toJson(test);
+    }
+
+    /**
+     * Sets some default headers for Http methods.
+     * @param req - Http request for which set default headers
+     */
+    protected static void setDefaultHeaders(HttpUriRequest req, String loadero_token) {
+        req.setHeader(HttpHeaders.ACCEPT, "application/json");
+        req.setHeader(HttpHeaders.CONTENT_TYPE, "application/json");
+        req.setHeader(HttpHeaders.AUTHORIZATION, "LoaderoAuth " + loadero_token);
+    }
 }
