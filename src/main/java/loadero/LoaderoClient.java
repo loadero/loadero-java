3dd1f05352dfb3abc9a54a26400b731f38ffa511
Mihhail Matišinets <mihhail.matisinets@testdevlab.com>
2021-02-04 09:53:59 +0200

    Added null checks, specific exceptions, formatting

--- src/main/java/loadero/LoaderoClient.java
+++ src/main/java/loadero/LoaderoClient.java
@@ -9,18 +9,102 @@ import org.apache.http.client.methods.HttpUriRequest;
 import org.apache.http.client.methods.RequestBuilder;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
 
+import java.io.UnsupportedEncodingException;
 import java.net.URI;
+import java.util.Objects;
 
+// TODO: Parse return value into java object - Done
+// TODO: Null checks
 public class LoaderoClient {
     private static final String BASE_URL = "https://api.loadero.com/v2";
     private static final String LOADERO_API_TOKEN = System.getenv("LOADERO_API_TOKEN");
     private static final String PROJECT_ID = "5040";
     private static final String TEST_ID = "6866";
+    private static final Gson gson = new Gson();
+    private static final HttpClientBuilder client = HttpClients.custom();
     // TODO: create some generic method to create uris.
     private static final URI uri = URI.create(BASE_URL+"/projects/"+PROJECT_ID+"/tests/"+TEST_ID+"/");
+
+    /**
+     * TODO: make return LoaderoTestDescription object.
+     * Returns test description for the specific test in specific project.
+     * @param projectId
+     * @param testId
+     */
+    public static LoaderoTestDescription getTestDescription(String projectId, String testId) {
+        LoaderoTestDescription test = null;
+        HttpUriRequest get = RequestBuilder.get(uri).build();
+        setDefaultHeaders(get);
+        // Try-catch with resources statement that will close
+        // everything for us after we are done.
+        try (CloseableHttpClient client = HttpClients.custom().build();
+             CloseableHttpResponse res = client.execute(get)) {
+            test = jsonToTestDescr(res.getEntity());
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return test;
+    }
+
+
+    // TODO: refactor, make more readable, add javadoc
+    public static LoaderoTestDescription updateTestDescription(String projectId,
+                                             String testId,
+                                             LoaderoTestDescription newTest) {
+        LoaderoTestDescription result = null;
+        if (checkNull(newTest)) {
+            throw new NullPointerException();
+        }
+        try {
+            String testToJson = testDescrToJson(newTest);
+            HttpEntity entity = new StringEntity(testToJson);
+            HttpUriRequest put = RequestBuilder.put(uri)
+                    .setEntity(entity)
+                    .build();
+            setDefaultHeaders(put);
+            try(CloseableHttpResponse res = client.build().execute(put)) {
+                if (res.getStatusLine().getStatusCode() == 200 &&
+                        !(checkNull(res.getEntity()))) {
+                    result = jsonToTestDescr(res.getEntity());
+                } else {
+                    System.out.println(res.getStatusLine());
+                }
+            } catch (UnsupportedEncodingException e) {
+                System.out.println(e.getLocalizedMessage());
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        LoaderoTestDescription getTest = getTestDescription(PROJECT_ID, TEST_ID);
+        LoaderoTestDescription updateTest = updateTestDescription(PROJECT_ID, TEST_ID, null);
+        System.out.println(updateTest);
+    }
+
+    private static boolean checkNull(Object test) {
+        return Objects.isNull(test);
+    }
+    // Converts JSON from response into LoaderTestDescription object
+    private static LoaderoTestDescription jsonToTestDescr(HttpEntity entity) {
+        LoaderoTestDescription test = null;
+        try {
+            test = gson.fromJson(EntityUtils.toString(entity), LoaderoTestDescription.class);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return test;
+    }
+
+    private static String testDescrToJson(LoaderoTestDescription test) {
+        return gson.toJson(test);
+    }
 
     /**
      * Sets some default headers for Http methods.
@@ -30,57 +114,5 @@ public class LoaderoClient {
         req.setHeader(HttpHeaders.ACCEPT, "application/json");
         req.setHeader(HttpHeaders.CONTENT_TYPE, "application/json");
         req.setHeader(HttpHeaders.AUTHORIZATION, "LoaderoAuth " + LOADERO_API_TOKEN);
-    }
-
-    /**
-     * TODO: make return LoaderoTestDescription object.
-     * Returns test description for the specific test in specific project.
-     * @param projectId
-     * @param testId
-     */
-    public static void getTestDescription(String projectId, String testId) {
-        HttpUriRequest get = RequestBuilder.get(uri).build();
-        setDefaultHeaders(get);
-        // Try-catch with resources statement that will close
-        // everything for us after we are done.
-        try (CloseableHttpClient client = HttpClients.custom().build();
-             CloseableHttpResponse res = client.execute(get)) {
-            HttpEntity entity = res.getEntity();
-            System.out.println(EntityUtils.toString(entity));
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
-    }
-
-    // TODO
-    public static void updateTestDescription(String projectId, String testId) {
-        LoaderoTestDescription test = new LoaderoTestDescription("New name 2",
-                30, 10,
-                "load", "random", "new script");
-        Gson gson = new Gson();
-        try (CloseableHttpClient client = HttpClients.custom().build()){
-            HttpEntity entity = new StringEntity(gson.toJson(test));
-            System.out.println("Object: " + entity);
-            System.out.println("Object: " + gson.toJson(test));
-            HttpUriRequest put = RequestBuilder.put(uri)
-                    .setEntity(entity)
-                    .build();
-            setDefaultHeaders(put);
-            CloseableHttpResponse res = client.execute(put);
-
-            if (res.getStatusLine().getStatusCode() != 200) {
-                System.out.println(res.getStatusLine().getReasonPhrase());
-            } else {
-                System.out.println("Successfully updated.");
-            }
-
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
-    }
-
-    public static void main(String[] args) {
-        getTestDescription(PROJECT_ID, TEST_ID);
-        updateTestDescription(PROJECT_ID, TEST_ID);
     }
 }
