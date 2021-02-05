104264613e52fd80ff84385bc6a000ed1baeb9b6
Mihhail Matišinets <mihhail.matisinets@testdevlab.com>
2021-02-05 09:31:45 +0200

    file for storing TestUI test snippets

--- src/test/java/TestParser.java
+++ src/test/java/TestParser.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class TestParser {
+import org.junit.Test;
+
+import static testUI.UIOpen.open;
+import static testUI.Utils.AppiumHelps.sleep;
+import static testUI.Utils.By.byCssSelector;
+import static testUI.Utils.By.byId;
+
+public class TestParser {
+
+    @Test
+    public void test() {
+        // Example of locating elements using CSS selector and ID
+        open("https://appr.tc/")
+                // Wait 10 seconds until "body" element is visible
+                .setElement(byCssSelector("body")).waitFor(10).untilIsVisible()
+                // Find "join button" and click on it
+                .setElement(byId("join-button")).click();
+
+        // Wait for 10 seconds
+        sleep(10 * 1000);
+    }
 }
