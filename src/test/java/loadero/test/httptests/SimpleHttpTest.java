fd074f3c6f8dc232a4c5af0db0094fa01f065a85
Mihhail Matišinets <mihhail.matisinets@testdevlab.com>
2021-02-06 21:00:12 +0200

    Added new tests to check new methods for LoaderClient class

--- src/test/java/loadero/test/httptests/SimpleHttpTest.java
+++ src/test/java/loadero/test/httptests/SimpleHttpTest.java
@@ -2,21 +2,53 @@ package loadero.test.httptests;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import loadero.LoaderoClient;
+import loadero.model.LoaderoGroup;
+import loadero.model.LoaderoParticipant;
 import loadero.model.LoaderoTestOptions;
+import org.apache.hc.client5.http.classic.methods.HttpGet;
+import org.apache.hc.client5.http.classic.methods.HttpUriRequest;
+import org.apache.hc.client5.http.impl.classic.HttpClientBuilder;
+import org.apache.hc.core5.http.HttpResponse;
+import org.apache.hc.core5.http.HttpStatus;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
 import java.net.URI;
 
 
 public class SimpleHttpTest {
     private static final String token = System.getenv("LOADERO_API_TOKEN");
-    private static final LoaderoClient client = new LoaderoClient(token);
+    private static final String GROUP_ID = "48797";
+    private static final String PROJECT_ID = "5040";
+    private static final String TEST_ID = "6866";
+    private static final String PARTICIPANT_ID = "94633";
+    private static final String RUNS_ID = "94633";
+    private static final LoaderoClient client = new LoaderoClient(token, PROJECT_ID, TEST_ID);
+
     private static final URI testUri = URI.create(
             client.getBASE_URL()
                     +"/projects/"
-                    + client.getPROJECT_ID()
+                    + client.getProjectId()
                     +"/tests/"
-                    +client.getTEST_ID() + "/");
+                    +client.getTestId() + "/");
+
+    @BeforeAll
+    public static void testIfApiIsUp() {
+        try {
+            HttpUriRequest request = new HttpGet(client.getBASE_URL() + "/docs");
+            HttpResponse response = HttpClientBuilder.create()
+                    .build().execute( request );
+            assertEquals(HttpStatus.SC_OK, response.getCode());
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    @BeforeAll
+    public static void checkIfLoaderoApiTokenPresent() {
+        assertEquals(token.getClass(), String.class);
+    }
 
     @Test
     public void testSpecificTestIdUrl() {
@@ -28,5 +60,21 @@ public class SimpleHttpTest {
     public void testGetTestOptions() {
         LoaderoTestOptions options = client.getTestOptions();
         assertEquals(options.getClass().getSimpleName(), LoaderoTestOptions.class.getSimpleName());
+    }
+
+    @Test
+    public void testGetGroupById() {
+        LoaderoGroup group = client.getGroupById(GROUP_ID);
+        assertEquals(group.getClass().getSimpleName(),
+                LoaderoGroup.class.getSimpleName());
+    }
+
+    @Test
+    public void testGetParticipantById() {
+        LoaderoParticipant participant = client.getParticipantById(
+                PARTICIPANT_ID, GROUP_ID);
+        System.out.println(participant);
+        assertEquals(participant.getClass().getSimpleName(),
+                LoaderoParticipant.class.getSimpleName());
     }
 }
