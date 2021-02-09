2c9790be8b90053c042141391aece9929c5eacea
Mihhail Matišinets <mihhail.matisinets@testdevlab.com>
2021-02-09 08:41:26 +0200

    formatting

--- src/main/java/loadero/model/LoaderoModelFactory.java
+++ src/main/java/loadero/model/LoaderoModelFactory.java
@@ -4,13 +4,13 @@ public class LoaderoModelFactory implements LoaderoModel {
 
     public LoaderoModel getLoaderoModel(LoaderoType type) {
         switch (type) {
-            case LOADERO_TEST_OPTIONS: return new LoaderoTestOptions();
-            case LOADERO_GROUP: return new LoaderoGroup();
-            case LOADERO_PARTICIPANT: return new LoaderoParticipant();
-            case LOADERO_RUN_INFO: return new LoaderoRunInfo();
-            case LOADERO_TEST_RESULT: return new LoaderoTestResult();
+            case LOADERO_TEST_OPTIONS:    return new LoaderoTestOptions();
+            case LOADERO_GROUP:           return new LoaderoGroup();
+            case LOADERO_PARTICIPANT:     return new LoaderoParticipant();
+            case LOADERO_RUN_INFO:        return new LoaderoRunInfo();
+            case LOADERO_TEST_RESULT:     return new LoaderoTestResult();
             case LOADERO_SCRIPT_FILE_LOC: return new LoaderoScriptFileLoc();
-            default: return null;
+            default:                      return null;
         }
     }
 }
