a6f52288ac6abbc322ec0179540a484ee3ad0215
Mihhail Matišinets <mihhail.matisinets@testdevlab.com>
2021-02-10 12:53:42 +0200

    Added some unit test with Wiremock

--- src/test/java/loadero/TestWithWireMock.java
+++ src/test/java/loadero/TestWithWireMock.java
@@ -1,80 +1,100 @@
 package loadero;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import com.github.tomakehurst.wiremock.WireMockServer;
 import com.github.tomakehurst.wiremock.client.MappingBuilder;
 import com.github.tomakehurst.wiremock.client.WireMock;
-import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
-import com.github.tomakehurst.wiremock.http.Response;
-import com.github.tomakehurst.wiremock.http.ResponseDefinition;
-import com.github.tomakehurst.wiremock.junit.WireMockClassRule;
 import com.github.tomakehurst.wiremock.junit.WireMockRule;
 import com.github.tomakehurst.wiremock.recording.SnapshotRecordResult;
+import com.github.tomakehurst.wiremock.stubbing.ServeEvent;
 import com.github.tomakehurst.wiremock.stubbing.StubMapping;
-import org.apache.http.HttpEntity;
+import com.google.gson.Gson;
+import loadero.utils.LoaderoHttpClient;
 import org.apache.http.HttpHeaders;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.impl.client.HttpClients;
-import org.junit.ClassRule;
+import org.apache.http.util.EntityUtils;
 import org.junit.Rule;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.Test.*;
+import org.junit.jupiter.api.*;
 
 import java.io.IOException;
+import java.util.List;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.*;
 
 public class TestWithWireMock {
-    private static final String token = System.getenv("LOADERO_API_TOKEN");
-    private static final String GROUP_ID = "48797";
+    private static final String token      = System.getenv("LOADERO_API_TOKEN");
     private static final String PROJECT_ID = "5040";
-    private static final String TEST_ID = "6866";
+    private static final String TEST_ID    = "6866";
     private static final LoaderoClient loaderoClient = new LoaderoClient(token,PROJECT_ID, TEST_ID);
-//    private static final WireMockServer server = new WireMockServer(8089);
-    private final String proxyTarget = loaderoClient.buildProjectURL();
+
+    private static final int port              = 8089;
+    private static final String localhost      = "http://localhost:" + port;
+    private final LoaderoHttpClient httpClient = new LoaderoHttpClient(token);
 
     @Rule
-    public WireMockRule wmRule = new WireMockRule(8089);
+    public static WireMockRule wmRule = new WireMockRule(8089);
 
-    @BeforeEach
-    public void setup() {
+    @BeforeAll
+    public static void setup() {
         wmRule.start();
     }
 
-    @AfterEach
-    public void teardown() {
+    @AfterAll
+    public static void teardown() {
         wmRule.stop();
     }
 
     @Test
-    public void basicWiremockTest() {
-        MappingBuilder mappingBuilder = get(urlMatching("/blah"));
-        wmRule.givenThat(mappingBuilder
+    public void testGetTestWithId() throws IOException {
+        String url = "/projects/"
+                + loaderoClient.getProjectId()
+                + "/tests/"
+                + loaderoClient.getTestId();
+
+        wmRule.givenThat(get(url)
                 .willReturn(aResponse()
-                        .withStatus(200)
                         .withHeader(HttpHeaders.CONTENT_TYPE, "application/json")
-                        .withStatusMessage("Everything is ok")))
-                .getResponse();
+                        .withStatus(200))
+        );
+        // Make GET request to url on localhost
+        HttpGet getTests = new HttpGet(localhost + url);
+        CloseableHttpResponse response = httpClient.build().execute(getTests);
 
-        CloseableHttpClient client = HttpClients.createDefault();
-        HttpGet get = new HttpGet("http://localhost:8089/blah");
-        HttpResponse res2 = null;
+        // Verify that Wiremock received request
+        wmRule.verify(getRequestedFor(urlPathMatching(url))
+                .withHeader(HttpHeaders.CONTENT_TYPE, equalTo("application/json")));
 
-        try {
-            res2 = client.execute(get);
-        } catch (IOException ex) {
-        }
+        assertEquals(200, response.getStatusLine().getStatusCode());
+        assertEquals("application/json", response.getEntity().getContentType().getValue());
+    }
 
-        wmRule.verify(getRequestedFor(urlEqualTo("/blah")));
-        assertEquals(200, res2.getStatusLine().getStatusCode());
-        assertEquals("application/json", res2
-                .getFirstHeader("Content-type").getValue());
+    @Test
+    public void testTokenAccess() throws IOException {
+        String url = "/projects/" + loaderoClient.getProjectId() +"/";
+        wmRule.givenThat(get(url)
+                .willReturn(aResponse()
+                        .withHeader(HttpHeaders.AUTHORIZATION, "LoaderoAuth " + token)
+                        .withStatus(200)));
+
+        HttpGet request = new HttpGet(localhost + url);
+        CloseableHttpResponse response = httpClient.build().execute(request);
+
+        wmRule.verify(getRequestedFor(urlPathMatching(url))
+                .withHeader(HttpHeaders.AUTHORIZATION, equalTo("LoaderoAuth " + token)));
+
+        assertEquals(HttpHeaders.AUTHORIZATION, response.getFirstHeader(HttpHeaders.AUTHORIZATION).getName());
+        assertEquals("LoaderoAuth " + token, response.getFirstHeader(HttpHeaders.AUTHORIZATION).getValue());
+    }
+
+    @Test
+    public void testGettingProjectByIdFromSavedMappings() throws IOException {
+        HttpGet get = new HttpGet(localhost + "/projects/" + loaderoClient.getProjectId() + "/");
+        CloseableHttpResponse response = httpClient.build().execute(get);
+        assertEquals(200, response.getStatusLine().getStatusCode());
     }
 }
