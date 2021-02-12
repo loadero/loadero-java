66e3fb2346385ccdbf4819faf36688c4bb8f539e
Mihhail Matišinets <mihhail.matisinets@testdevlab.com>
2021-02-12 12:39:06 +0200

    Added new unit tests with Wiremock and mock data

--- src/test/java/loadero/TestSimpleFunctionality.java
+++ src/test/java/loadero/TestSimpleFunctionality.java
@@ -20,24 +20,26 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TestSimpleFunctionality {
     private static final String token = System.getenv("LOADERO_API_TOKEN");
+    private static final String baseUrl = "https://api.loadero.com/v2";
     private static final String GROUP_ID = "48797";
     private static final String PROJECT_ID = "5040";
     private static final String TEST_ID = "6866";
     private static final String PARTICIPANT_ID = "94633";
     private static final String RUNS_ID = "94633";
-    private static final LoaderoClient client = new LoaderoClient(token, PROJECT_ID, TEST_ID);
+    private static final LoaderoClient client = new LoaderoClient(baseUrl, token, PROJECT_ID);
 
     private static final URI testUri = URI.create(
-            client.getBASE_URL()
+            client.getBaseUrl()
                     + "/projects/"
                     + client.getProjectId()
                     + "/tests/"
-                    + client.getTestId() + "/");
+                    + TEST_ID
+                    + "/");
 
     @BeforeAll
     public static void testIfApiIsUp() {
         try {
-            HttpUriRequest request = new HttpGet(client.getBASE_URL() + "/docs");
+            HttpUriRequest request = new HttpGet(client.getBaseUrl() + "/docs");
             HttpResponse response = HttpClientBuilder.create()
                     .build().execute( request );
             assertEquals(HttpStatus.SC_OK, response.getCode());
@@ -54,25 +56,25 @@ public class TestSimpleFunctionality {
     @Test
     public void testSpecificTestIdUrl() {
         assertEquals(testUri.toString(),
-                client.buildTestURL());
+                client.buildTestURLById(TEST_ID));
     }
 
     @Test
     public void testGetTestOptions() {
-        LoaderoModel options = client.getTestOptions();
+        LoaderoModel options = client.getTestOptionsById(TEST_ID);
         assertEquals(options.getClass().getSimpleName(), LoaderoTestOptions.class.getSimpleName());
     }
 
     @Test
     public void testGetGroupById() {
-        LoaderoModel group = client.getGroupById(GROUP_ID);
+        LoaderoModel group = client.getGroupById(TEST_ID, GROUP_ID);
         assertEquals(group.getClass().getSimpleName(),
                 LoaderoGroup.class.getSimpleName());
     }
 
     @Test
     public void testGetParticipantById() {
-        LoaderoModel participant = client.getParticipantById(
+        LoaderoModel participant = client.getParticipantById(TEST_ID,
                 PARTICIPANT_ID);
         assertEquals(participant.getClass().getSimpleName(),
                 LoaderoParticipant.class.getSimpleName());
--- src/test/java/loadero/TestWithWireMock.java
+++ src/test/java/loadero/TestWithWireMock.java
@@ -15,6 +15,7 @@ import org.apache.http.HttpHeaders;
 import org.apache.http.HttpStatus;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPut;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
@@ -31,180 +32,146 @@ import static org.junit.jupiter.api.Assertions.*;
 
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class TestWithWireMock {
-    private static final String token      = System.getenv("LOADERO_API_TOKEN");
-    private static final String PROJECT_ID = "5040";
-    private static final String TEST_ID    = "6866";
-    private static final LoaderoClient loaderoClient = new LoaderoClient(token, PROJECT_ID, TEST_ID);
-    private static final Logger logger = LogManager.getLogger(TestWithWireMock.class);
-
+    private static final String token           = System.getenv("LOADERO_API_TOKEN");
+    private static final String PROJECT_ID      = "5040";
+    private static final String TEST_ID         = "6866";
     private static String localhost             = "http://localhost:";
     private static final String loaderoTokenStr = "LoaderoAuth " + token;
     private final LoaderoHttpClient httpClient  = new LoaderoHttpClient(token);
+    private static final Logger logger = LogManager.getLogger(TestWithWireMock.class);
+    private CloseableHttpResponse response;
+    private static LoaderoClient loaderoClient;
+
+    /**
+     * Makes GET request to specified url and saves response to
+     * private response variable.
+      * @param url - URL to make request to.
+     */
+    private void makeGetRequest(String url) {
+        try {
+            HttpGet get = new HttpGet(url);
+            response = httpClient.build().execute(get);
+        } catch (IOException ex) {
+            logger.error("{}", ex.getMessage());
+        }
+    }
+
 
     @Rule
     public static WireMockRule wmRule = new WireMockRule(
             WireMockConfiguration.wireMockConfig().dynamicPort()
     );
-//
-//    @BeforeAll
-//    public static void setup() {
-//        wmRule.start();
-//        localhost = localhost + wmRule.port();
-//    }
-//
-//    @AfterAll
-//    public static void teardown() {
-//        wmRule.stop();
-//    }
+
+    @BeforeAll
+    public static void setup() {
+        wmRule.start();
+        localhost = localhost + wmRule.port();
+        loaderoClient = new LoaderoClient(localhost, token, PROJECT_ID);
+    }
+
+    @AfterAll
+    public static void teardown() {
+        wmRule.stop();
+    }
 
     @Test
     @Order(1)
-    public void testGetTestWithId() throws IOException {
-        String url = "/projects/"
-                + loaderoClient.getProjectId()
-                + "/tests/"
-                + loaderoClient.getTestId();
-
-        wmRule.givenThat(get(url)
-                .willReturn(aResponse()
-                        .withHeader(HttpHeaders.CONTENT_TYPE, "application/json")
-                        .withStatus(200))
-        );
-        // Make GET request to url on localhost
-        HttpGet getTests = new HttpGet(localhost + url);
-        CloseableHttpResponse response = httpClient.build().execute(getTests);
-
-        // Verify that Wiremock received request
-        wmRule.verify(getRequestedFor(urlPathMatching(url))
-                .withHeader(HttpHeaders.CONTENT_TYPE, equalTo("application/json")));
+    public void testGetTestOptions() throws IOException {
+        // Make GET request to url on localhost to get status code
+        makeGetRequest(loaderoClient.buildTestURLById(TEST_ID) + "/");
+        LoaderoTestOptions test = loaderoClient.getTestOptionsById(TEST_ID);
+        String jsonRes = EntityUtils.toString(response.getEntity());
 
         assertEquals(200,
                 response.getStatusLine().getStatusCode());
-        assertEquals("application/json",
+        assertEquals("application/json;charset=utf-8",
                 response.getEntity().getContentType().getValue());
+        assertEquals(TEST_ID, String.valueOf(test.getId()));
+        assertTrue(jsonRes.contains(TEST_ID));
     }
 
     @Test
     @Order(2)
-    public void testTokenAccess() throws IOException {
-        String url = "/projects/" + loaderoClient.getProjectId() +"/";
-        wmRule.givenThat(get(urlMatching(url))
-                .willReturn(aResponse()
-                        .proxiedFrom(loaderoClient.getBASE_URL())
-                        .withAdditionalRequestHeader(
-                                HttpHeaders.AUTHORIZATION,
-                                loaderoTokenStr)));
-
-        HttpGet request = new HttpGet(localhost + url);
-        CloseableHttpResponse response = httpClient.build().execute(request);
-        String jsonRes = EntityUtils.toString(response.getEntity());
-
-        wmRule.verify(getRequestedFor(urlPathMatching(url))
-                .withHeader(HttpHeaders.AUTHORIZATION,
-                        equalTo("LoaderoAuth " + token)));
-
-        assertTrue(jsonRes.contains(loaderoClient.getProjectId()));
+    public void negativeGetTestOptionsByUrl() {
+        String url = loaderoClient.buildTestURLById("0");
+        makeGetRequest(url);
+        assertEquals(HttpStatus.SC_NOT_FOUND, response.getStatusLine().getStatusCode());
     }
 
     @Test
     @Order(3)
-    public void negativeTokenAccess() throws IOException {
-        String url = "/projects/" + loaderoClient.getProjectId() +"/";
+    public void testTokenAccessByUrl() {
+        String url = loaderoClient.buildProjectURL() + "/";
 
-        wmRule.givenThat(get(urlMatching(url))
-                .willReturn(aResponse()
-                        .proxiedFrom(loaderoClient.getBASE_URL())));
-
-        HttpGet request = new HttpGet(localhost + url);
-
-        CloseableHttpClient client = HttpClients.createDefault();
-        CloseableHttpResponse response = client.execute(request);
-
-        assertEquals(HttpStatus.SC_UNAUTHORIZED, response.getStatusLine().getStatusCode());
+        makeGetRequest(url);
+        assertEquals(HttpStatus.SC_OK, response.getStatusLine().getStatusCode());
     }
 
     @Test
     @Order(4)
-    public void testGetTestOptionsProxyFromLoaderApi() throws IOException {
-        String url = "/projects/"
-                + loaderoClient.getProjectId()
-                + "/tests/"
-                + loaderoClient.getTestId()+"/";
-
-        wmRule.givenThat(get(urlMatching(url))
-                .willReturn(aResponse()
-                        .proxiedFrom(loaderoClient.getBASE_URL())
-                        .withAdditionalRequestHeader(
-                                HttpHeaders.AUTHORIZATION,
-                                loaderoTokenStr)));
-
+    public void negativeTokenAccess() throws IOException {
+        String url = "/projects/";
         HttpGet request = new HttpGet(localhost + url);
-        CloseableHttpResponse response = httpClient.build().execute(request);
-        String jsonRes = EntityUtils.toString(response.getEntity());
-
-        assertEquals(HttpStatus.SC_OK, response.getStatusLine().getStatusCode());
-        assertTrue(jsonRes.contains(loaderoClient.getTestId()));
-    }
-
-    @Test
-    @Disabled
-    public void negativeGetTestOptionsFromLoaderoApi() throws IOException {
-        String url = "/projects/"
-                + loaderoClient.getProjectId()
-                + "/tests/"
-                + "9"
-                +"/";
-
-        StubMapping stub = wmRule.givenThat(get(urlPathMatching(url))
-                .willReturn(aResponse()
-                        .proxiedFrom(loaderoClient.getBASE_URL())));
-
-        logger.info("{}", url);
-        assertEquals(HttpStatus.SC_NOT_FOUND, stub.getResponse().getStatus());
+        CloseableHttpClient client = HttpClients.createDefault();
+        response = client.execute(request);
+        assertEquals(HttpStatus.SC_FORBIDDEN, response.getStatusLine().getStatusCode());
     }
 
     @Test
     @Order(5)
-    public void testGettingProjectByIdFromSavedMappings() throws IOException {
-        HttpGet get = new HttpGet(localhost + "/projects/" + loaderoClient.getProjectId() +"/");
-        CloseableHttpResponse response = httpClient.build().execute(get);
+    public void testGettingProjectFromSavedMappings() throws IOException {
+        makeGetRequest(loaderoClient.buildProjectURL() + "/");
+        String jsonRes = EntityUtils.toString(response.getEntity());
         assertEquals(HttpStatus.SC_OK, response.getStatusLine().getStatusCode());
+        assertTrue(jsonRes.contains(loaderoClient.getProjectId()));
     }
 
     @Test
     @Order(6)
-    public void testGetParticipantById() {
-        String participantId = "94633";
-        LoaderoParticipant participant =
-                loaderoClient.getParticipantById(participantId);
-        assertEquals(participantId, String.valueOf(participant.getId()));
+    public void negativeTestGettingProjectFromSavedMappings() {
+        makeGetRequest(localhost + "/projects/" + "0" + "/");
+        assertEquals(HttpStatus.SC_NOT_FOUND, response.getStatusLine().getStatusCode());
     }
 
     @Test
-    public void testUpdateParticipantById() {
+    @Order(7)
+    public void testGetParticipantById() {
         String participantId = "94633";
-        // name, group_id, count and browser params are required
-        LoaderoParticipant newParticipant = new LoaderoParticipant();
-        newParticipant.setName("New participant name from @Test");
+        String url = loaderoClient.buildParticipantURL(TEST_ID, participantId) + "/";
 
-        LoaderoParticipant updatedParticipant = loaderoClient
-                .updateTestParticipantById(participantId, newParticipant);
+        LoaderoParticipant participant = loaderoClient.getParticipantById(TEST_ID, participantId);
+        makeGetRequest(url);
 
-        assertEquals("New participant name from @Test", updatedParticipant.getName());
-        assertEquals(participantId, String.valueOf(updatedParticipant.getId()));
+        assertEquals(HttpStatus.SC_OK, response.getStatusLine().getStatusCode());
+        assertEquals(participantId, String.valueOf(participant.getId()));
+        assertEquals(TEST_ID, String.valueOf(participant.getTestId()));
     }
+
+    // TODO
+    @Test
+    @Order(8)
+    @Disabled
+    public void testUpdateParticipantById() throws IOException {
+        String participantId = "94633";
+    }
+
+    // Add new tests before this comment
+    // Don't forget to change order!
 
     @Test
     @Disabled
-    @Order(8)
+    @Order(9)
     public void testFullFunctionalityFlow() {
-        LoaderoClient localClient = new LoaderoClient(token, PROJECT_ID, TEST_ID);
-        String testClientInitUrl = localClient.buildProjectURL();
+        String baseUrl = "https://api.loadero.com/v2";
+        LoaderoClient localClient = new LoaderoClient(
+                baseUrl, token,
+                PROJECT_ID);
+        String testClientInitUrl = localClient.buildProjectURL() + "/";
 
         // Checking that client can establish connection and make requests
         StubMapping stub = wmRule.givenThat(get(urlMatching(testClientInitUrl))
                 .willReturn(aResponse()
-                        .proxiedFrom(localClient.getBASE_URL())
+                        .proxiedFrom(localClient.getBaseUrl())
                         .withAdditionalRequestHeader(
                                 HttpHeaders.AUTHORIZATION,
                                 "LoaderoAuth " + localClient.getLoaderoApiToken()
@@ -212,7 +179,7 @@ public class TestWithWireMock {
 
         assertEquals(HttpStatus.SC_OK, stub.getResponse().getStatus());
         // Checking that client is able to update test
-        LoaderoTestOptions currentTestOptions = loaderoClient.getTestOptions();
+        LoaderoTestOptions currentTestOptions = localClient.getTestOptionsById(TEST_ID);
         LoaderoTestOptions newTestOptions = new LoaderoTestOptions();
         newTestOptions.setName("New Test Name from @Test");
         // Setting script through path using URI
@@ -220,7 +187,7 @@ public class TestWithWireMock {
                 URI.create("src/main/resources/loadero/scripts/testui/LoaderoScriptJava.java"));
         newTestOptions.setMode("performance");
         newTestOptions.setStartInterval(30);
-        LoaderoTestOptions updatedTestOptions = localClient.updateTestOptions(newTestOptions);
+        LoaderoTestOptions updatedTestOptions = localClient.updateTestOptions(TEST_ID, newTestOptions);
 
         logger.info("Before update: {}", currentTestOptions);
         logger.info("After update: {}", updatedTestOptions);
@@ -228,7 +195,7 @@ public class TestWithWireMock {
         assertEquals("New Test Name from @Test", updatedTestOptions.getName());
 
         // Checking polling function
-        LoaderoRunInfo startAndPollTest = loaderoClient.startTestAndPollInfo(20, 40);
+        LoaderoRunInfo startAndPollTest = localClient.startTestAndPollInfo(TEST_ID, 20, 40);
         assertEquals("done", startAndPollTest.getStatus());
     }
 }
