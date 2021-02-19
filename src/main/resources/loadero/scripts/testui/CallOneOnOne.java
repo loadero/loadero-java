618f768f289ca615aadca94c0e4a4f68d70f0fee
Mihhail Matišinets <mihhail.matisinets@testdevlab.com>
2021-02-19 17:35:54 +0200

    Added Loadero scripts

--- src/main/resources/loadero/scripts/testui/CallOneOnOne.java
+++ src/main/resources/loadero/scripts/testui/CallOneOnOne.java
@@ -1,28 +1,33 @@
-public class LoaderoScriptJava {
-
-    @Test
-    public void test() {
-        int particId     = globalConfig.getParticipant().getId();
-        String identity  = String.format("Participant%d", particId);
-        String toCall    = "";
-        String appUrl    = "";
-        int callDuration = 60; // 1 minute
-
-        updateNetwork(loaderoConstants.getNetwork().getDefault());
+public void test() {
+        int particId = globalConfig.getParticipant().getId();
+        String identity = String.format("Participant%d", particId);
+        int callDuration = 60; // in seconds
+        String appUrl = "https://voice-webapp-3026-dev.twil.io/index.html?identity=";
+        int elTimeout = 10;
 
         if (particId % 2 == 0) {
-            toCall = "Participant2";
-            appUrl = "https://voice-webapp-3026-dev.twil.io/index.html?identity=" + identity;
-            open(appUrl)
-                    .setElement(byCssSelector("body")).waitFor(10).untilIsVisible()
-                    .setElement(byId("phone-number")).waitFor(10).untilIsVisible().then().sendKeys(toCall)
-                    .setElement(byId("button-call")).waitFor(10).untilIsVisible().click();
-        } else {
-            String recieveCall = "Participant2";
-            appUrl = "https://voice-webapp-3026-dev.twil.io/index.html?identity=" + recieveCall;
-            open(appUrl);
-        }
+            String caller = String.format("%s%s", appUrl, identity);
+            String toCall = String.format("Participant%d", particId+1);
 
-        sleep(callDuration*1000);
-    }
+            open(caller)
+                .setElement(byCssSelector("body")).waitFor(elTimeout).untilIsVisible().shouldBe().visible()
+                .setElement(byId("phone-number")).waitFor(elTimeout).untilIsVisible().shouldBe().visible()
+                .setElement(byId("call-controls")).waitFor(elTimeout).untilIsVisible().shouldBe().visible()
+                .setElement(byId("phone-number")).waitFor(elTimeout).untilIsVisible()
+                .sendKeys(toCall)
+                .setElement(byId("button-call")).waitFor(elTimeout).untilIsVisible().click()
+                .setElement(byId("button-hangup")).waitFor(elTimeout).untilIsVisible()
+                .setElement(byId("log")).shouldHave().containNoCaseSensitiveText("Successfully established call!")
+                .setElement(byId("button-hangup")).waitFor(callDuration*1000).click();
+        } else {
+            String callee = String.format("%s%s", appUrl, toCall);
+            open(callee)
+                .setElement(byCssSelector("body")).waitFor(elTimeout).untilIsVisible()
+                .setElement(byId("phone-number")).waitFor(elTimeout).untilIsVisible()
+                .click("phone-number")
+                .setElement(byId("log")).waitFor(elTimeout).untilIsVisible().shouldHave().containNoCaseSensitiveText("Ready")
+                .setElement(byId("button-hangup")).waitFor(elTimeout).untilIsVisible()
+                .setElement(byId("log")).waitFor(callDuration*1000).shouldHave().containNoCaseSensitiveText("Call ended");
+        }
 }
+
